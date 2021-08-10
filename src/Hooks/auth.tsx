491b5bf3795806d3f0d28f5448e1c2d9195c5f90
Gabriel de Oliveira <gabriel@MacBook-Pro-de-Gabriel.local>
2021-08-10 16:21:52 -0700

    gofinances

--- src/Hooks/auth.tsx
+++ src/Hooks/auth.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
 import * as AuthSession from 'expo-auth-session';
 
 interface AuthProviderProps {
@@ -17,33 +17,49 @@ interface AuthContextProps {
   singInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
 const AuthContext = createContext({} as AuthContextProps);
 
 function AuthProvider({ children }: AuthProviderProps) {
-    const user = {
-      id: '3216456321',
-      name: 'Gabriel Oliveira',
-      email: 'gabriel@gmail.com',
-    }
+    const [user, setUser ] = useState<User>({} as User)
 
 
   async function singInWithGoogle() {
     try {
-      const CLIENT_ID = 'ar508r805iqktc0m15f2bh6bjrhis0uf.apps.googleusercontent.com';
+      const CLIENT_ID = '253350342402-ar508r805iqktc0m15f2bh6bjrhis0uf.apps.googleusercontent.com';
       const REDIRECT_URI = 'https://auth.expo.io/@gabrieldevelopers/gofinances';
       const RESPONSE_TYPE = 'token';
       const SCOPE = encodeURI('profile email');
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      AuthSession.startAsync({ authUrl });
+     const { type, params } = await AuthSession
+     .startAsync({ authUrl })as AuthorizationResponse;
+
+     if(type === 'success'){
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`);
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          name: userInfo.given_name,
+          email: userInfo.email,
+          photo: userInfo.picture,
+        });
+      }
+
 
     }catch (error) {
       throw new Error(error);
     }
-  }
+  } 
 
-  
   return(
     <AuthContext.Provider value={{ 
       user,
